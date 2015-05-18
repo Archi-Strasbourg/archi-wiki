c48518e8a480dda2d1b8995e0537bb74ce483a85
rotagraziosi <antoine.rota.Graziosi@gmail.com>
2015-05-18 17:10:17 +0200

    Test full export

--- modules/opendata/fullexport.php
+++ modules/opendata/fullexport.php
@@ -1,16 +1,19 @@
 <?php
 foreach (glob("lib/ArchiWikiConvertor/Interface/*.php") as $filename){
-	echo $filename."<br/>";
-	include $filename;
+	var_dump(__DIR__);
+	var_dump($filename);
+	include __DIR__.DIRECTORY_SEPARATOR.$filename;
 }
 foreach (glob("lib/ArchiWikiConvertor/*.php") as $filename){
+	var_dump(__DIR__);
 	var_dump($filename);
-	include $filename;
-}
+	include __DIR__.DIRECTORY_SEPARATOR.$filename;
+	}
 foreach (glob("lib/*.php") as $filename){
+	var_dump(__DIR__);
 	var_dump($filename);
-	include $filename;
-}
+	include __DIR__.DIRECTORY_SEPARATOR.$filename;
+	}
 
 ini_set('memory_limit', '-1');
 set_time_limit(0);
