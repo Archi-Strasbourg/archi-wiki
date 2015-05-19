62e618cefc38fbdc0b9c5a69f69d603e2f2d1e0a
rotagraziosi <antoine.rota.Graziosi@gmail.com>
2015-05-19 16:22:42 +0200

    Remove dirty db access to resize image

--- resizeImage.php
+++ resizeImage.php
@@ -10,25 +10,30 @@
  * @link     https://archi-strasbourg.org/
  * 
  * */
+require_once "includes/framework/config.class.php";
+$config = new Config();
+
 $path="images/placeholder.jpg";
 if (isset($_GET["id"]) && !empty($_GET["id"])) {
-	$ressource = mysql_connect('localhost', 'archi_user', 'archi_pwd')
+	/*$ressource = mysql_connect('localhost', 'archi_user', 'archi_pwd')
 	or die(file_get_contents(__DIR__.'/../../../maintenance.html'));
 	mysql_select_db('archi_reboot') or die(mysql_error());
 	mysql_query('SET NAMES "utf8"') or die (mysql_error());
+	*/
 	$req = "
             SELECT dateUpload
             FROM  historiqueImage
             WHERE idHistoriqueImage = '".mysql_real_escape_string($_GET["id"])."'";
-	
-	$res = mysql_query($req)
+	$res=$config->connexionBdd->requete($req);
+	/*$res = mysql_query($req)
 	or
 	die($requete.' -- '.mysql_error().' -- <br/> Request in file : <b>'.debug_backtrace()[0]['file'].'</b><br/> on line <b>'.debug_backtrace()[0]['line']).'</b>';
+	*/
 	//$res =$config->connexionBdd->requete($req);
 	$image=mysql_fetch_object($res);
 	if ($image) {
 		//http://archi-strasbourg.org/photo-detail-16_rue_bastian__cronenbourg__strasbourg-1-evenement-idEvenement-1-adresse1.html
-		$tempPath="images/grand/".$image->dateUpload."/".$_GET["id"].".jpg";
+		$tempPath="http://archi-strasbourg.org/photos--".$image->dateUpload."-".$_GET["id"]."-grand.jpg";
 		//if (file_exists($tempPath)) {
 		$path = $tempPath;
 		// }
