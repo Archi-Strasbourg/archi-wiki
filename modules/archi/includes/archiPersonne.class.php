10293f6d3eb828dfaa380fefd146fd397b8da41d
Pierre Rudloff <contact@rudloff.pro>
2017-01-26 15:42:07 +0100

    Display full address in linked event on person page

--- modules/archi/includes/archiPersonne.class.php
+++ modules/archi/includes/archiPersonne.class.php
@@ -949,6 +949,102 @@ class ArchiPersonne extends ArchiContenu
                     }
                 }
 
+                $idEvenementGroupeAdresse = $a->getIdEvenementGroupeAdresseFromIdAdresse($linkedEventIdAddress);
+
+                $reqAdresseDuGroupeAdresse = "
+                    SELECT ha1.idAdresse as idAdresse,ha1.numero as numero,
+                    ha1.idRue as idRue, IF(ha1.idIndicatif='0','',i.nom)
+                    as nomIndicatif, ha1.idQuartier as idQuartier, ha1.idSousQuartier as idSousQuartier
+                    FROM historiqueAdresse ha2, historiqueAdresse ha1
+                    LEFT JOIN _adresseEvenement ae ON ae.idAdresse = ha1.idAdresse
+                    LEFT JOIN indicatif i ON i.idIndicatif = ha1.idIndicatif
+                    WHERE ha2.idAdresse = ha1.idAdresse
+                    AND ae.idEvenement ='".mysql_real_escape_string($idEvenementGroupeAdresse)."'
+
+                    GROUP BY ha1.idAdresse, ha1.idHistoriqueAdresse
+                    HAVING ha1.idHistoriqueAdresse = max(ha2.idHistoriqueAdresse)
+                    ORDER BY ha1.numero,ha1.idRue
+                ";
+
+                $resAdresseDuGroupeAdresse = $a->connexionBdd->requete($reqAdresseDuGroupeAdresse);
+
+                if (mysql_num_rows($resAdresseDuGroupeAdresse) > 1) {
+                    $txtAdresses = '';
+                    $arrayNumero = [];
+                    while ($fetchAdressesGroupeAdresse = mysql_fetch_assoc($resAdresseDuGroupeAdresse)) {
+                        $isAdresseCourante = false;
+                        if ($address['idAdresse'] == $fetchAdressesGroupeAdresse['idAdresse']) {
+                            $isAdresseCourante = true;
+                        }
+
+                        if ($fetchAdressesGroupeAdresse['idRue'] == '0' || $fetchAdressesGroupeAdresse['idRue'] == '') {
+                            if ($fetchAdressesGroupeAdresse['idQuartier'] != ''
+                                && $fetchAdressesGroupeAdresse['idQuartier'] != '0'
+                            ) {
+                                $arrayNumero[$this->a->getIntituleAdresseFrom(
+                                    $fetchAdressesGroupeAdresse['idAdresse'],
+                                    'idAdresse',
+                                    ['noSousQuartier' => true, 'noQuartier' => false, 'noVille' => true]
+                                )][] =
+                                    [
+                                        'indicatif'         => $fetchAdressesGroupeAdresse['nomIndicatif'],
+                                        'numero'            => $fetchAdressesGroupeAdresse['numero'],
+                                        'isAdresseCourante' => $isAdresseCourante,
+                                    ];
+                            }
+
+                            if ($fetchAdressesGroupeAdresse['idSousQuartier'] != ''
+                                && $fetchAdressesGroupeAdresse['idSousQuartier'] != '0'
+                            ) {
+                                $arrayNumero[$this->a->getIntituleAdresseFrom(
+                                    $fetchAdressesGroupeAdresse['idAdresse'],
+                                    'idAdresse',
+                                    ['noSousQuartier' => false, 'noQuartier' => true, 'noVille' => true]
+                                )][] =
+                                    [
+                                        'indicatif'         => $fetchAdressesGroupeAdresse['nomIndicatif'],
+                                        'numero'            => $fetchAdressesGroupeAdresse['numero'],
+                                        'isAdresseCourante' => $isAdresseCourante,
+                                    ];
+                            }
+                        } else {
+                            $arrayNumero[$a->getIntituleAdresseFrom(
+                                $fetchAdressesGroupeAdresse['idRue'],
+                                'idRueWithNoNumeroAuthorized',
+                                ['noSousQuartier' => true, 'noQuartier' => true, 'noVille' => true]
+                            )][] =
+                                [
+                                    'indicatif'         => $fetchAdressesGroupeAdresse['nomIndicatif'],
+                                    'numero'            => $fetchAdressesGroupeAdresse['numero'],
+                                    'isAdresseCourante' => $isAdresseCourante,
+                                ];
+                        }
+                    }
+
+                    // affichage adresses regroupees
+                    foreach ($arrayNumero as $intituleRue => $arrayInfosNumero) {
+                        foreach ($arrayInfosNumero as $indice => $infosNumero) {
+                            if ($infosNumero['numero'] == '' || $infosNumero['numero'] == '0') {
+                                //rien
+                            } else {
+                                $txtAdresses .= $infosNumero['numero'].$infosNumero['indicatif'].'-';
+                            }
+                        }
+
+                        $txtAdresses = trim($txtAdresses, '-');
+
+                        $txtAdresses .= $intituleRue.', ';
+                    }
+                    $txtAdresses = trim($txtAdresses, ', ').
+                        str_replace(
+                            $a->getIntituleAdresseFrom($linkedEvent, "idEvenement", ['noQuartier'=>true, 'noSousQuartier'=>true, 'noVille'=>true]),
+                            '',
+                            $linkedEventAddress
+                        );
+                } else {
+                    $txtAdresses = $linkedEventAddress;
+                }
+
                 //WIP sélection de l'image des personnes
                 /*$req = "
                         SELECT  nom
@@ -977,7 +1073,7 @@ class ArchiPersonne extends ArchiContenu
                 }
                 $linkedEventUrl=$config->creerUrl("", "adresseDetail", array("archiIdAdresse"=>$linkedEventIdAddress, "archiIdEvenementGroupeAdresse"=>$linkedEvent));
                 $linkedEventsHTML.="<li class='linkedEvents'><img src='".
-                $linkedEventImg["url"]."' alt='' /> <div style='display:inline-block;'><a href='$linkedEventUrl'>".$linkedEventAddress;
+                $linkedEventImg["url"]."' alt='' /> <div style='display:inline-block;'><a href='$linkedEventUrl'>".$txtAdresses;
                 $res=$e->getInfosEvenementsLiesForAncres($e->getIdEvenementGroupeAdresseFromIdEvenement($linkedEvent));
                 $i=0;
                 while ($rep=mysql_fetch_object($res)) {
